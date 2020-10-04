95c0914c29a95f4ff770d768cc50792e8186f455
dacanizares <dacanizares@outlook.com>
2020-10-04 01:52:40 -0500

    Updated command

--- _run-dev.ps1
+++ _run-dev.ps1
@@ -1,5 +1,6 @@
 cd .\themes\tranquilpeak
 invoke-expression 'cmd /c start powershell -Command { npm run start }'
+Read-Host -Prompt "Wait until npm start completes"
 cd..
 cd..
 hugo server -w --noHTTPCache --disableFastRender -v
