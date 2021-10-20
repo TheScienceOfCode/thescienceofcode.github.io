1802f6c461a889ad56bf2872ab38c18b8d93c806
dacanizares <dacanizares@outlook.com>
2021-10-20 00:27:00 -0500

    Updated script

--- _scripts/update-theme.ps1
+++ _scripts/update-theme.ps1
@@ -1,1 +1,7 @@
-git submodule update --remote --merge
+. $PSScriptRoot/_EquilaterusCLI.ps1
+
+Function Invoke-Script {
+    git submodule update --remote --merge
+}
+
+Start-Cli -Title 'Update theme' -Filename 'config.toml'
