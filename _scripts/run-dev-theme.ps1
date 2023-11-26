55e7808cfae005d6863504e45a69a4068768c095
Daniel Cañizares Corrales <dacanizares@outlook.com>
2023-11-25 23:39:14 -0500

    Deleted _scripts directory

--- _scripts/run-dev-theme.ps1
+++
@@ -1,8 +0,0 @@
-. $PSScriptRoot/_EquilaterusCLI.ps1
-
-Function Invoke-Script {
-    Set-Location '.\themes\tranquilpeak'
-    npm run start
-}
-
-Start-Cli -Title 'Run Theme' -Filename 'config.toml'
